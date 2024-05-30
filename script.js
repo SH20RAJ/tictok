2ad21be8f677af02c41c194f1da052be876a3bc3
sh20raj <shaswatraj3@gmail.com>
2024-05-30 20:49:07 +0530

    tic tac toe tashi

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+let possiblities  = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+];
+
+let tashi = (a) => document.querySelector(a);
+
+
+// tashi bkl
+tashi("#1").innerHTML = "bkl";
+
+
+
+
+
+
